300e2885bc3ea98eda4f8d81ee919a8dd826fd45
yb7553 <23218371@qq.com>
2019-08-08 19:28:07 +0700

    kk

--- vagrant/labs/1.sh
+++ vagrant/labs/1.sh
@@ -11,5 +11,5 @@ docker tag registry.aliyuncs.com/google_containers/kube-scheduler:v1.15.1 k8s.gc
 docker tag registry.aliyuncs.com/google_containers/kube-controller-manager:v1.15.1 k8s.gcr.io/kube-controller-manager:v1.15.1
 docker tag registry.aliyuncs.com/google_containers/kube-proxy:v1.15.1 k8s.gcr.io/kube-proxy:v1.15.1
 docker tag registry.aliyuncs.com/google_containers/etcd:3.3.10 k8s.gcr.io/etcd:3.3.10
-docker tag registry.aliyuncs.com/google_containers/coredns:1.3.1 k8s.gcr.io/coreos/coredns:1.3.1
+docker tag registry.aliyuncs.com/google_containers/coredns:1.3.1 k8s.gcr.io/coredns:1.3.1
 docker tag registry.aliyuncs.com/google_containers/pause:3.1 k8s.gcr.io/pause:3.1
